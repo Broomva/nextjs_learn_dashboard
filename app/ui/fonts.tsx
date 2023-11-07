2a24da9376c1a822ebcfc4a3b850f08469182ed2
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2023-11-06 21:15:40 -0500

    Added changes up to chapter 3

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,11 @@
+import { Assistant, Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+    weight: ["400", "700"],
+    subsets: ["latin"],
+});
+export const assistant = Assistant({
+    weight: ["400"],
+    subsets: ["latin"],
+});
